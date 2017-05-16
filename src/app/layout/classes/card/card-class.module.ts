9ce445b737d6f0dc98817986f5b0f960885bcb27
Iván J. Pérez <synpheros@hotmail.com>
2017-05-16 18:25:48 +0200

    Included Session State Module into class cards

--- src/app/layout/classes/card/card-class.module.ts
+++ src/app/layout/classes/card/card-class.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { SessionStateModule } from '../../sessions/state/session-state.module';
 
 import { CardClassComponent } from './card-class.component';
 import { GamesService } from '../../../_services/games.service';
@@ -9,7 +10,8 @@ import { GamesService } from '../../../_services/games.service';
 @NgModule({
     imports: [
         CommonModule,
-        FormsModule
+        FormsModule,
+        SessionStateModule
     ],
     declarations: [CardClassComponent],
     exports: [CardClassComponent],
